d6ae10bd00f1c08454ebda9077aa3e1840b71b11
aanya101010 <aanya.singh901@gmail.com>
2025-09-02 18:58:14 -0500

    A3_HelloUser.java

--- A3_HelloUser.java
+++ A3_HelloUser.java
@@ -1,16 +1,15 @@
-
 import java.util.Scanner;
 
 public class A3_HelloUser {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Aanya: ");
+        System.out.println("Enter your name:");
         String name = scanner.nextLine();
 
-        System.out.println("Hello " + Aanya + ".");
+        System.out.println(); // print blank line
+        System.out.println("Hello " + name + ".");
 
         scanner.close();
     }
 }
-
