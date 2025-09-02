a4046e38d44d7f19fadf570882f09a915a6f33fa
aanya101010 <aanya.singh901@gmail.com>
2025-09-02 18:40:38 -0500

    Add files via upload

---
+++ A3_HelloUser.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+public class A3_HelloUser {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Aanya: ");
+        String name = scanner.nextLine();
+
+        System.out.println("Hello " + "Aanya" + ".");
+
+        scanner.close();
+    }
+}
+
